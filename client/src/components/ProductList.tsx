f49e12541131985e8612fbe621649c729e27a2b1
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2026-01-28 18:14:36 +0530

    Add Filter component and integrate it into ProductList for sorting functionality

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { ProductsType } from "../types";
 import Categories from "./Categories";
 import ProductCard from "./ProductCard";
+import Filter from "./Filter";
 
 // TEMPORARY
 const products: ProductsType = [
@@ -125,6 +126,7 @@ const ProductList = ({
   return (
     <div className="w-full">
       <Categories />
+      {params === "products" && <Filter />}
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
