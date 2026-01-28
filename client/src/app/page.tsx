5de41a1cdc74de9c495428f89fc8822290758503
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2026-01-28 09:58:34 +0530

    Add ProductList, ProductCard, and Categories components; update Homepage to include ProductList

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,4 @@
+import ProductList from "@/components/ProductList";
 import Image from "next/image";
 
 const Homepage = async ({
@@ -11,6 +12,7 @@ const Homepage = async ({
       <div className="relative aspect-[3/1] mb-12">
         <Image src="/featured.png" alt="Featured Product" fill />
       </div>
+      <ProductList />
     </div>
   );
 };
